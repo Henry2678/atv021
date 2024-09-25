a639b4e925b6654feaacaace6d2093d611630e74
Henry2678 <soldadohuefl@gmail.com>
2024-09-25 13:19:41 -0300

    atividade21

---
+++ atividade21.py
@@ -0,0 +1,7 @@
+# Crie um programa que faça uma contagem regressiva de 10 até 0 e exiba "FIM!" ao final.
+# Contagem regressiva de 10 a 0
+for i in range(10, -1, -1):
+    print(i)
+
+# Exibe "FIM!" ao final
+print("FIM!")
